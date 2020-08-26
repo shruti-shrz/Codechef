5d54855c3b6b86949389d014a680e755cc38e2c7
shobhi1310 <42507632+shobhi1310@users.noreply.github.com>
2020-08-26 08:58:00 +0530

    canaccess codes now

--- shubhankar/filler.py
+++ shubhankar/filler.py
@@ -4,7 +4,7 @@ import sqlite3
 import os
 import csv
 
-parser = argparse.ArgumentParser(description='Scrape codechef problem')
+parser = argparse.ArgumentParser(description='analyze codechef problem')
 parser.add_argument('-d', '--database', default='analyze.db')
 parser.add_argument('-f', '--fill', default='../dataset/analyzer.csv')
 args = parser.parse_args()
@@ -12,9 +12,27 @@ args = parser.parse_args()
 conn = sqlite3.connect(args.database)
 c = conn.cursor()
 
-c.execute('SELECT code FROM programs WHERE program_id = 34852142')
 
-result = c.fetchall()
+with open(args.fill) as file:
+    reader = csv.reader(file,delimiter=',')
+    lc = 0
+    for row in reader:
+        if lc!=0:
+            if(row[7]=='C++14'):
+                t = (row[0],'C++14')
+                c.execute('SELECT code FROM programs WHERE program_id =?',t)
+                result = c.fetchone()
+                if result != None:
+                    # print(row[0])
+                    for x in result:
+                        print(x)
+        lc += 1
 
-for x in result:
-    print(x[0])
+conn.close()
+
+# c.execute('SELECT code FROM programs WHERE program_id =34854309')
+# result = c.fetchone()
+# if result != None:
+#     print("Nope this aint the place")
+# for x in result:
+#     print(x)
